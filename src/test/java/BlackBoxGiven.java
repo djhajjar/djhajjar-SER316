f7bf9878cad73ade7c98f28d88cf2a32f1efcd18
Daniel Hajjar <djhajjar@gmail.com>
2020-03-29 08:49:25 -0500

    Reformating for readability

--- src/test/java/BlackBoxGiven.java
+++ src/test/java/BlackBoxGiven.java
@@ -21,62 +21,55 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class BlackBoxGiven {
 
-    private Class<Cart> classUnderTest;
+	private Class<Cart> classUnderTest;
 
-    @SuppressWarnings("unchecked")
-    public BlackBoxGiven(Object classUnderTest) {
-        this.classUnderTest = (Class<Cart>) classUnderTest;
-    }
+	@SuppressWarnings("unchecked")
+	public BlackBoxGiven(Object classUnderTest) {
+		this.classUnderTest = (Class<Cart>) classUnderTest;
+	}
 
-    // Define all classes to be tested
-    @Parameterized.Parameters
-    public static Collection<Object[]> cartClassUnderTest() {
-        Object[][] classes = {
-            {Cart0.class},
-            {Cart1.class},
-            {Cart2.class},
-            {Cart3.class},
-            {Cart4.class},
-            {Cart5.class}
-        };
-        return Arrays.asList(classes);
-    }
+	// Define all classes to be tested
+	@Parameterized.Parameters
+	public static Collection<Object[]> cartClassUnderTest() {
+		Object[][] classes = { { Cart0.class }, { Cart1.class }, { Cart2.class }, { Cart3.class }, { Cart4.class },
+				{ Cart5.class } };
+		return Arrays.asList(classes);
+	}
 
-    private Cart createCart(int age) throws Exception {
-        Constructor<Cart> constructor = classUnderTest.getConstructor(Integer.TYPE);
-        return constructor.newInstance(age);
-    }
+	private Cart createCart(int age) throws Exception {
+		Constructor<Cart> constructor = classUnderTest.getConstructor(Integer.TYPE);
+		return constructor.newInstance(age);
+	}
 
-    // A sample Cart
+	// A sample Cart
 
-    Cart cart1;
-    double cart1Expected;
+	Cart cart1;
+	double cart1Expected;
 
+	@org.junit.Before
+	public void setUp() throws Exception {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
+		// all carts should be set up like this
 
-        // all carts should be set up like this
+		// cart created with an age 40 shopper
+		cart1 = createCart(40);
+		for (int i = 0; i < 2; i++) {
+			cart1.addItem(new Alcohol());
+		}
+		for (int i = 0; i < 3; i++) {
+			cart1.addItem(new Dairy());
+		}
+		for (int i = 0; i < 4; i++) {
+			cart1.addItem(new Meat());
+		}
 
-        // cart created with an age 40 shopper
-        cart1 = createCart(40);
-        for (int i = 0; i < 2; i++) {
-            cart1.addItem(new Alcohol());
-        }
-        for(int i = 0; i < 3; i++) {
-            cart1.addItem(new Dairy());
-        }
-        for(int i = 0; i < 4; i++) {
-            cart1.addItem(new Meat());
-        }
+		cart1Expected = 70.2;
+	}
 
-        cart1Expected = 70.2;
-    }
-
-    // sample test
-    @Test
-    public void calcCostCart1() throws UnderAgeException {
-        double amount = cart1.calcCost();
-        assertEquals(cart1Expected, amount, .01);
-    }
+	// sample test
+	@Test
+	public void calcCostCart1() throws UnderAgeException {
+		double amount = cart1.calcCost();
+		assertEquals(cart1Expected, amount, .01);
+	}
 }
