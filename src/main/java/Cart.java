ddc2f242e3c0f70204f266e32b7b16a554f09e9a
Daniel Hajjar <djhajjar@gmail.com>
2020-05-01 19:32:26 -0500

    Assignment 6 Task 3.2 - Both Amount_saved() and calcCost() has been updated in order to lower NPath complexity to within the threshold and with cyclomatic complexity lowered to 5 from 10 and 12 respectfully. calcCost() now uses Amount_saved() to calculate the discount rather than finding the discount itself. Formulas were used to calculate the discount rather than counters and if-statements. Though Amount_saved() does not check for underage shoppers and simply calculates the amount saved, this check is still performed in calcCost().

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -9,6 +9,7 @@ public class Cart {
 
     protected int userAge;
     public List<Product> cart;
+
     // SER316 Task 2 SPOTBUGS FIX - removed cartStorage
 
     /**
@@ -16,8 +17,11 @@ public class Cart {
      * Also checks that the user is of age to purchase alcohol if it is in their
      * cart at checkout. Sales tax is always AZ tax.
      *
-     * Calculation is based off of the following prices and deals: Dairy -> $3
-     * Meat -> $10 Produce -> $2 or 3 for $5 Alcohol -> $8 Frozen Food -> $5
+     * Calculation is based off of the following prices and deals: <br>
+     * Dairy -> $3 <br>
+     * Meat -> $10 <br>
+     * Produce -> $2 or 3 for $5 <br>
+     * Alcohol -> $8 Frozen Food -> $5 <br>
      * Alcohol + Frozen Food -> $10
      *
      * If there is an alcohol product in the cart and the user is under 21, then
@@ -29,44 +33,15 @@ public class Cart {
     public double calcCost() throws UnderAgeException {
         double cost = 0;
 
-        int produceCounter = 0;
-        int alcoholCounter = 0;
-        int frozenCounter = 0;
-
         for (Product product : cart) {
             cost += product.getCost();
 
-            if (product instanceof Produce) {
-                produceCounter++;
-
-                if (produceCounter >= 3) {
-                    cost -= 1;
-                    produceCounter = 0;
-                }
-            } else if (product instanceof Alcohol) {
-                alcoholCounter++;
-
-                if (this.userAge < 21) {
-                    throw new UnderAgeException("Cart Owner must be 21 or"
-                            + " over to purchase alcohol");
-                }
-
-                if (alcoholCounter >= 1 && produceCounter >= 1) {
-                    cost -= 3;
-                    alcoholCounter--;
-                    produceCounter--;
-                }
-            } else if (product instanceof FrozenFood) {
-                frozenCounter++;
-
-                if (alcoholCounter >= 1 && frozenCounter >= 1) {
-                    cost -= 3;
-                    alcoholCounter--;
-                    frozenCounter--;
-                }
+            if (product instanceof Alcohol && this.userAge < 21) {
+                throw new UnderAgeException("Cart Owner must be 21 or over to purchase alcohol");
             }
         }
 
+        cost -= this.Amount_saved();
         return cost + this.getTax(cost, "AZ");
     }
 
@@ -76,43 +51,25 @@ public class Cart {
     // TODO: Create node graph for this method in assign 4: create white box
     // tests and fix the method, reach at least 98% coverage
     public int Amount_saved() throws UnderAgeException {
-        int subTotal = 0;
-        int costAfterSavings = 0;
-
+        int discount = 0;
         double produce_counter = 0;
         int alcoholCounter = 0;
         int frozenFoodCounter = 0;
 
         for (int i = 0; i < cart.size(); i++) {
-            subTotal += cart.get(i).getCost();
-            costAfterSavings += cart.get(i).getCost();
-
             if (cart.get(i) instanceof Produce) {
                 produce_counter++;
-
-                if (produce_counter >= 3) {
-                    costAfterSavings -= 1;
-                    produce_counter = 0;
-                }
             } else if (cart.get(i) instanceof Alcohol) {
                 alcoholCounter++;
-                if (userAge < 21) {
-                    throw new UnderAgeException("The User is not of age to purchase alcohol!");
-                }
             } else if (cart.get(i) instanceof FrozenFood) {
                 frozenFoodCounter++;
             }
-
-            // removed excess code that was tracking dairy counter
-
-            if (alcoholCounter >= 1 && frozenFoodCounter >= 1) {
-                costAfterSavings -= 3; // added instead of subtracted
-                alcoholCounter--;
-                frozenFoodCounter--;
-            }
         }
 
-        return subTotal - costAfterSavings;
+        discount += (int) Math.floor(produce_counter / 3);
+        discount += Math.min(alcoholCounter, frozenFoodCounter) * 3;
+
+        return discount;
     }
 
     // Gets the tax based on state and the total
